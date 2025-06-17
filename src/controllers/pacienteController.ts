4761657d95e200952e2e33fb751f386a30c70b7f
Willian de Oliveira Ribeiro <will.oliveira.2408@gmail.com>
2025-06-17 07:30:57 -0300

    add novo campo "idade"

--- src/controllers/pacienteController.ts
+++ src/controllers/pacienteController.ts
@@ -1,17 +1,17 @@
 import {Request, Response} from 'express';
+import {db} from '../config/database';
 import {Paciente} from '../models/Paciente';
 
 export const cadastrarPaciente =  async (req: Request, res: Response) => {
-    
+
     try {
-        const paciente: Paciente = req.body
-
-        console.log('Dados recebidos', paciente);
-
-
+        
+        const {cpf, nome, email, senha, telefone, genero, idade} = req.body as Paciente
+        console.log(req.body);
+        console.log(cpf);
+        
         return res.status(201).json({
             message: 'Paciente cadastrado com sucesso!',
-            paciente: paciente
         });  
     } catch (error) {
         return res.status(500).json({
