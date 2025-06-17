910c812b99f3ced6b332627573b2919640493b68
Willian de Oliveira Ribeiro <will.oliveira.2408@gmail.com>
2025-06-17 00:35:50 -0300

    post do formulario de cadastro realizado com sucesso

--- src/routes/pacienteRota.ts
+++ src/routes/pacienteRota.ts
@@ -3,6 +3,12 @@ import {cadastrarPaciente} from '../controllers/pacienteController'
 
 const router = express.Router();
 
+
+const app = express()
+// Este middleware é ESSENCIAL para parsear o body das requisições
+app.use(express.json());
+
+
 router.post('/cadastroPaciente', (req, res, next) => {
     cadastrarPaciente(req, res).catch(next)});
 
