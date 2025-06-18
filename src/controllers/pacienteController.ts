a2f2642282ee6bbedb8bd273f3cbe21ad5fa09fb
Willian de Oliveira Ribeiro <will.oliveira.2408@gmail.com>
2025-06-18 07:16:22 -0300

    add hash nas senhas

--- src/controllers/pacienteController.ts
+++ src/controllers/pacienteController.ts
@@ -1,17 +1,22 @@
 import {Request, Response} from 'express';
 import {db} from '../config/database';
 import {Paciente} from '../models/Paciente';
+import bcrypt from 'bcrypt';
+
 
 export const cadastrarPaciente =  async (req: Request, res: Response) => {
 
     try {
         
         const {cpf, nome, email, senha, telefone, genero, idade} = req.body as Paciente
+
+        const senhaCriptografada = await bcrypt.hash(senha, 10);
+
         console.log(`Dados recebidos ${req.body}`);
 
         const [resultado] = await db.execute (
             'INSERT INTO pacientes (cpf, nome, email, senha, telefone, genero, idade) VALUES (?,?,?,?,?,?,?)',
-            [cpf,nome,email,senha,telefone,genero,idade]
+            [cpf,nome,email,senhaCriptografada,telefone,genero,idade]
         )
         
         console.log("Paciente cadastrado com sucesso!"); //resposta de sucesso (backend)
