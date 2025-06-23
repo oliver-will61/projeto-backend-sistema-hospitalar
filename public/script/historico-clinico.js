8b8c557b983c8bc8dc49666a2fcd5ff6fe76f72f
Willian de Oliveira Ribeiro <will.oliveira.2408@gmail.com>
2025-06-23 07:24:32 -0300

    add front-end historico

--- public/script/historico-clinico.js
+++ public/script/historico-clinico.js
@@ -2,3 +2,33 @@ function mostraFormulario(){
     const form = document.getElementById("form")
     form.style.display = 'block'
 }
+
+async function enviaDatabase(event) {
+
+    event.preventDefault()
+    
+    const alergias = [document.querySelectorAll('input[name="alergias"]:checked')]
+        .map(cb => cb.value);
+
+    const doencas = [document.querySelectorAll('input[name="doencas"]:checked')]
+    .map(cb => cb.value);
+
+
+    const historico = {
+        alergias: alergias,
+        doencasCronicas: doencas
+    };
+
+    console.log(historico)
+
+    // const response = await fetch('/api/paciente/historico', {
+    //     method:'POST',
+    //     headers: {'Content-type': 'application/json'},
+    //     body: JSON.stringify(historico)
+    // })
+
+    // const result = await response.json(); // mostra a resposta do servidor
+    // console.log(result);
+    
+
+}
