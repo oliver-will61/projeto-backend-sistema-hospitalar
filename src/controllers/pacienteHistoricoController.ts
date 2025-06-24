6c68697ebf79a11e49a920047112a26bf1c0d6d1
Willian de Oliveira Ribeiro <will.oliveira.2408@gmail.com>
2025-06-24 20:47:41 -0300

    dados do historico clinico indo para o db

--- src/controllers/pacienteHistoricoController.ts
+++ src/controllers/pacienteHistoricoController.ts
@@ -4,13 +4,38 @@ import {db} from '../config/database';
 export const historicoClinicoPaciente = async (req: Request, res: Response) => {
     
     try{
-        const historico = req.body 
-        console.log(historico);
+
+        const  {alergia, doencaCronica} = req.body
+
+        console.log(req.body);
+        
+
+        const alergiaJson = JSON.stringify(alergia)
+        const doencaCronicaJson = JSON.stringify(doencaCronica)
+
+
+        console.log(alergiaJson);
+        console.log(doencaCronicaJson);
+        
+        
+
+        const [result] = await db.execute(
+            'INSERT INTO historico_clinico (alergia, doenca_cronica) VALUES (?,?)',
+            [alergiaJson, doencaCronicaJson]
+        )
+
+        console.log('Dados enviados para o banco');
+        
+
+        res.status(201).json({
+            sucesso: true,
+            mensage: 'Enviados com sucesso ao banco'
+        })
         
     }
 
-    catch(erro) {
-        console.error(erro);
-        return
+    catch(error) {
+        console.error('Erro no servidor:', error);
+        res.status(500).json({ success: false, error: 'Erro ao salvar histórico' });
     }
 }
