144b8815f2adff118ecda75dc97f17a879f17655
Willian de Oliveira Ribeiro <will.oliveira.2408@gmail.com>
2025-06-25 07:06:20 -0300

    refatorado interaface usuario para classe

--- src/models/Paciente.ts
+++ src/models/Paciente.ts
@@ -1,15 +1,4 @@
-export class Paciente implements Usuario {
+export class Paciente extends Usuario {
 
-    constructor(
-        public cpf: string, public nome: string, public email: string, public senha: string, public telefone: string, 
-        public genero: string, public idade: number)
-    {
-        this.cpf = cpf;
-        this.nome = nome;
-        this.email = email;
-        this.senha = senha;
-        this.telefone = telefone;
-        this.genero = genero;
-        this.idade = idade
-    }
+    
 }
--- src/models/Usuario.ts
+++ src/models/Usuario.ts
@@ -1,9 +1,16 @@
-interface Usuario {
-    cpf: string
-    nome:string;
-    email: string;
-    senha: string;
-    telefone: string;
-    genero: string;
-    idade: number
+class Usuario {
+
+    constructor(
+        public cpf: string, public nome: string, public email: string, public senha: string, public telefone: string, 
+        public genero: string, public idade: number) 
+    {
+        this.cpf = cpf;
+        this.nome = nome;
+        this.email = email;
+        this.senha = senha;
+        this.telefone = telefone;
+        this.genero = genero;
+        this.idade = idade; 
+    }
+
 }
